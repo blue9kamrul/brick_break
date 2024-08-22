e6da438064d099c27cb4e33d5aa49b2abc90fa16
kamrul islam <cdkamrul9@gmail.com>
2024-08-22 22:01:44 +0600

    player movement added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+
 import 'dart:async';
 
 
@@ -5,6 +6,7 @@ import 'package:brick_break/ball.dart';
 import 'package:brick_break/coverscreen.dart';
 import 'package:brick_break/player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -32,39 +34,71 @@ class _HomePageState extends State<HomePage> {
   //start game func
   void startGame(){
     hasGameStarted = true;
-    Timer.periodic(Duration(milliseconds: 10), (timer) {
+    Timer.periodic(const Duration(milliseconds: 10), (timer) {
       setState(() {
-      ballY -= 0.01;
+      ballY += 0.001;
       });
+    });
+  }
+
+//move left
+  void moveLeft(){
+    setState(() {
+      if(!(playerX - .2 < -1.0)){
+        playerX -= 0.2;
+      }
+      
+    });
+  }
+  //move right
+  void moveRight(){
+    setState(() {
+      if(!(playerX + .2 > 1.0)){
+        playerX += 0.2;
+      }
     });
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: startGame,
-      child: Scaffold(
-      backgroundColor: Color.fromARGB(255, 192, 175, 196),
-      body: Center(
-        child:  Stack(
-          children: [
-            //tap to play
-            CoverScreen(
-              hasGameStarted: hasGameStarted),
-
-            //ball
-            MyBall(
-              ballX: ballX,
-              ballY: ballY,
-              ),
-              //player
-              MyPlayer(
-                playerX: playerX,
-                playerWidth: playerWidth,
-              )
-
-            ],  
+    return KeyboardListener(
+      focusNode: FocusNode(),
+      onKeyEvent: (event){
+        if(event is KeyDownEvent){
+          if(event.logicalKey == LogicalKeyboardKey.arrowLeft){
+            moveLeft();
+          }
+          if(event.logicalKey == LogicalKeyboardKey.arrowRight){
+            moveRight();
+          }
+        }
+      },
+      
+      child: GestureDetector(
+        onTap: startGame,
+        child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 192, 175, 196),
+        body: Center(
+          child:  Stack(
+            children: [
+              //tap to play
+              CoverScreen(
+                hasGameStarted: hasGameStarted),
+      
+              //ball
+              MyBall(
+                ballX: ballX,
+                ballY: ballY,
+                ),
+                //player
+                MyPlayer(
+                  playerX: playerX,
+                  playerWidth: playerWidth,
+                )
+      
+              ],  
+            ),
           ),
         ),
       ),
